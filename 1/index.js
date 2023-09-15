69b06c82f7e8e4a3c4ec7ec2102a10529a5d3e41
Suad Suljovic <suad.suljovic92@gmail.com>
2023-09-15 19:29:02 +0200

    created the 3rd gen project

---
+++ 1/index.js
@@ -0,0 +1,76 @@
+// primitives ✅
+// complex types ✅
+// const let ✅
+// operators ✅
+// logical operators ✅
+// conditionals ✅
+// loops ✅
+// objects ✅
+// arrays ✅
+// array methods ✅
+// callbacks ✅
+// promises
+// async await
+// closure
+// hoisting
+// classes
+// DOM
+// DOM manipulation
+// events
+// es6 js
+
+// var a = "";
+
+// console.log(typeof a);
+
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// console.log(sum(1, 2));
+// console.log(sum({}, []));
+
+// console.log(1 + 2 + "1", "prvi");
+// console.log("22" + 2 + "1", "drugi");
+// console.log(true + 2 + "1", "treci");
+// console.log(false + 2 + "1", "cetvrti");
+// console.log(true + true + "1", "peti");
+
+// var c = "";
+// const a = "";
+// let b = "";
+
+// var a = "string";
+
+// console.log(document, window);
+
+// console.log(window.a);
+
+// var a = 'sdfsdf'
+
+// console.log("" || false || true);
+// console.log("yes" || 0 || true);
+
+// console.log("a" && null && "b");
+// console.log("a" && "c" && "b");
+
+// const o = {};
+// const n = [];
+// o.property1 = "";
+
+// const obj = { size: 0, width: "22" };
+
+// // ako se na objektu nalazi property vratite vrednost
+// // ako se ne nalazi vratite -1
+// function getProperty(obj, property) {
+//   return typeof obj[property] == "undefined" ? obj[property] : -1;
+// }
+
+// console.log(getProperty(obj, "size"));
+
+const n = [1, 2, 3, 4];
+
+n.forEach((element, index, array) => {});
+n.filter();
+n.map();
+n.reduce();
