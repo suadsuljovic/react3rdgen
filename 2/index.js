7e701e98e66cbaa3b0a73455d0bd3c691e2607b6
Suad Suljovic <suad.suljovic92@gmail.com>
2023-09-15 21:01:57 +0200

    class 2

---
+++ 2/index.js
@@ -0,0 +1,109 @@
+// promises ✅
+// event loop ✅
+// async await ✅
+// DOM & BOM ✅
+// DOM manipulation  ✅
+// events ✅
+
+// hoisting ✅
+// closure ✅
+// classes ✅
+// es6 js ✅
+
+// http & fetch
+
+// prompt("sdfsdf");
+
+// console.log(nesto);
+
+// console.log("after error");
+
+// setTimeout(() => {
+//   console.log(nesto);
+// });
+
+// const promise = new Promise((resolve, reject) => {
+//   resolve("text");
+// });
+
+// promise.then((text) => {
+//   console.log(text);
+// });
+
+// console.log("after timeout");
+
+// const func = async () =>{
+//     try {
+
+//         const result = await fetch()
+
+//         console.log(reuuslt)
+
+//     } catch (error) {
+
+//     }
+// }
+
+// func()
+
+// console.log(window.document);
+// console.log(document);
+
+// const element = document.getElementById("nekiE");
+
+// element.innerText = "ovo sam promenio";
+// console.log(element);
+
+// const element = document.getElementById("nekiE");
+
+// element.addEventListener("click", () => {
+//   element.innerText = "clicked";
+// });
+
+// function onMouseEnter() {
+//   element.style.border = "1px solid black";
+// }
+
+// console.log(a);
+
+// console.log(name, a);
+
+// function name(params) {}
+
+// var a = function n(params) {};
+
+// console.log(a);
+
+// const a = "";
+
+// function sum(a) {
+//   return (b) => {
+//     c++;
+//     console.log(c);
+//     return a + b;
+//   };
+// }
+
+// const sumA = sum(2);
+// const sumB = sum(3);
+
+// console.log(sumA(2), sumA(4));
+// console.log(sumB(5));
+
+// class Auto {
+//   name;
+//   constructor(name) {
+//     this.name = name;
+//   }
+
+//   printName() {
+//     console.log(this.name);
+//   }
+// }
+
+// const auto1 = new Auto("BMW");
+// auto1.printName();
+
+// console.log(auto1.name);
+
+const a = fetch("").then((result) => result.json());
